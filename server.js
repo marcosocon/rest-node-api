21a1c55c5830cfc743081846328d4e357384ece1
marcosocon <oconmarcos97@gmail.com>
2016-12-01 20:41:09 -0300

    added mongoose connection and model

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require("path");
 var bodyParser = require('body-parser');
 var app = express();
+var Book = require('./models/bookModel');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
 
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
 	.get(function(req, res){
-		var getBooksJson = {hello: "This is my first JSON response from the api!"}
-		res.json(getBooksJson);
+		Book.find(function(err, books){
+			if(!err){
+				res.json(books);
+			} else {
+				res.status(500).send(err);
+			}
+		});
 	})
 
 app.use('/api', bookRouter);
