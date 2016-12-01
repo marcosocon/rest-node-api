f9e92e740273e4bc39cc020498a6f155c2aa4690
marcosocon <oconmarcos97@gmail.com>
2016-12-01 19:46:34 -0300

    added gulp task and first routes

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ var path = require("path");
 var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('Hello!');
-});
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+	.get(function(req, res){
+		var getBooksJson = {hello: "This is my first JSON response from the api!"}
+		res.json(getBooksJson);
+	})
+
+app.use('/api', bookRouter);
 
 var port = process.env.PORT || 8080;
 
 app.listen(port, function(){
-	console.log('Server running on port ' + port);
+	console.log('Gulp is running the app in port  ' + port);
 });
