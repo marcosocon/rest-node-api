f0b7dde5ce0d8f37cd32bfd8ab94b58cb234965a
marcosocon <oconmarcos97@gmail.com>
2017-02-13 23:56:47 -0300

    fixes in routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,15 +3,15 @@ var express = require('express');
 //ROUTES FOR BOOKS.
 var routes = function(Book){
 	var bookRouter = express.Router();
-	bookRouter.use('/:bookId', function(req, res){
-		Book.findById(req.params.bookId, function(err, book, next){
+	bookRouter.use('/books/:bookId', function(req, res, next){
+		Book.findById(req.params.bookId, function(err, book){
 			if(err){
 				res.status(500).send(err);
 			} else if(book) {
 				res.book = book;
 				next();
 			} else {
-				res.status(404).send('Book not found.')
+				res.status(404).send('Book not found.');
 			}
 		});
 	});
@@ -39,15 +39,15 @@ var routes = function(Book){
 		});
 	bookRouter.route('/books/:bookId')
 		.put(function(req, res){
-			req.book.title = req.body.title
-			req.book.author = req.body.author
-			req.book.genre = req.body.genre
-			req.book.read = req.body.read;
-			req.book.save(function(err){
+			res.book.title = req.body.title;
+			res.book.author = req.body.author;
+			res.book.genre = req.body.genre;
+			res.book.read = req.body.read;
+			res.book.save(function(err){
 				if(err){
 					res.status(500).send(err);
 				} else {
-					res.json(req.book);
+					res.json(res.book);
 				}
 			});
 		})
@@ -56,19 +56,19 @@ var routes = function(Book){
 				delete req.body._id;
 			}
 			for(var i in req.body){
-				req.book[i] = req.body[i];
+				res.book[i] = req.body[i];
 			}
-			req.book.save(function(err){
+			res.book.save(function(err){
 				if(err){
 					res.status(500).send(err);
 				} else {
-					res.json(req.book);
+					res.json(res.book);
 				}
 			});
 		})
 		.get(function(req, res){
-			res.json(req.book);
+			res.json(res.book);
 		});
 	return bookRouter;
-}
+};
 module.exports = routes;
